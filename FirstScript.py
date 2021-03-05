08c9ae5924220cbf5d5de24ca2a7a74b6c94f0ef
David <72844238+APCweiwei@users.noreply.github.com>
2021-03-05 16:02:58 +0800

    Create FirstScript.py

---
+++ FirstScript.py
@@ -0,0 +1,19 @@
+import time
+
+
+class TimeTest(object):
+
+    def __init__(self, hour, minute, second):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+
+    @staticmethod
+    def showTime(cls):
+        return time.strftime("%H:%M:%S", time.localtime())
+
+
+print(TimeTest.showTime())
+t = TimeTest(2, 10, 10)
+nowTime = t.showTime()
+print(nowTime)
